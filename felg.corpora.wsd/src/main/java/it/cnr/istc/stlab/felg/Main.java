f194d2e92bd8593853f37d59f4621b0914683faf
Luigi Asprino <luigi.asprino@gmail.com>
2020-04-11 09:52:27 +0200

    not ready print

--- felg.corpora.wsd/src/main/java/it/cnr/istc/stlab/felg/Main.java
+++ felg.corpora.wsd/src/main/java/it/cnr/istc/stlab/felg/Main.java
@@ -69,6 +69,7 @@ public class Main {
 			// wait until the wsd is initialized
 			while (!r.isReady()) {
 				Thread.sleep(1000);
+				logger.trace("Not ready");
 			}
 
 			logger.info("WSD initialized");
