decedadda4c0df723998a7f4d776c61e15bdbb11
Luigi Asprino <luigi.asprino@gmail.com>
2020-09-22 16:13:54 +0200

    csv writer

--- felg.corpus/src/main/java/it/cnr/istc/stlab/felg/corpus/Analysis.java
+++ felg.corpus/src/main/java/it/cnr/istc/stlab/felg/corpus/Analysis.java
@@ -1,12 +1,15 @@
 package it.cnr.istc.stlab.felg.corpus;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.text.NumberFormat;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -18,6 +21,8 @@ import org.apache.commons.configuration2.builder.fluent.Configurations;
 import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+
+import com.opencsv.CSVWriter;
 
 import it.cnr.istc.stlab.lgu.commons.io.FileUtils;
 import it.cnr.istc.stlab.lgu.commons.lang.BitMask;
@@ -39,21 +44,29 @@ public class Analysis {
 			String setFilePath = config.getString("setFilePath");
 			String frameListPath = config.getString("frameListPath");
 			String uniqueFramesetsPath = config.getString("uniqueFramesetsPath");
+			String uniqueFramesetsPathCSV = config.getString("uniqueFramesetsPathCSV");
 
 			NumberFormat nf = NumberFormat.getInstance();
 
-//			Map<String, long[]> framesToBitMask = frameSetToBitMask(frameListPath);
+			Map<String, long[]> framesToBitMask = frameSetToBitMask(frameListPath);
 
+			BufferedWriter writer = Files.newBufferedWriter(Paths.get(uniqueFramesetsPathCSV));
+
+			CSVWriter csvw = new CSVWriter(writer, ',', CSVWriter.NO_QUOTE_CHARACTER, CSVWriter.NO_ESCAPE_CHARACTER,
+					CSVWriter.DEFAULT_LINE_END);
 			BufferedReader br = new BufferedReader(new FileReader(new File(setFilePath)));
 			OutputStream fos = new FileOutputStream(new File(uniqueFramesetsPath));
 			int linenumber = 0;
 			String line;
 			String[] cols;
-//			String[] frames;
-//			long[][] frameMasks;
-//			long[] framesetMask;
+			String[] frames;
+			long[][] frameMasks;
+			long[] framesetMask;
 //			int hashCodeFramesetMask;
-//			String bitmaskString;
+			String bitmaskString;
+			String[] header = getHeader(frameListPath);
+			int numberOfFrames = header.length;
+			csvw.writeNext(header);
 			while ((line = br.readLine()) != null) {
 				if (linenumber % 100000 == 0) {
 
@@ -62,21 +75,26 @@ public class Analysis {
 				}
 
 				cols = line.split("\t");
-//				frames = cols[1].split(" ");
-//				frameMasks = new long[frames.length][];
-//				for (int i = 0; i < frames.length; i++) {
-//					frameMasks[i] = framesToBitMask.get(frames[i]);
-//				}
-//				framesetMask = BitMask.or(frameMasks);
+				frames = cols[1].split(" ");
+				frameMasks = new long[frames.length][];
+				for (int i = 0; i < frames.length; i++) {
+					frameMasks[i] = framesToBitMask.get(frames[i]);
+				}
+				framesetMask = BitMask.or(frameMasks);
 //				hashCodeFramesetMask = framesetMask.hashCode();
 //				bitmaskString = BitMask.longBitMaskToString(framesetMask);
 				if (!uniqueFrameSets.contains(cols[0].hashCode())) {
-					fos.write(line.getBytes());
+					bitmaskString = BitMask.longBitMaskToString(framesetMask);
+					bitmaskString = bitmaskString.substring(bitmaskString.length() - numberOfFrames);
+					fos.write(bitmaskString.getBytes());
 					fos.write('\n');
+					csvw.writeNext(bitmaskString.split(""));
 					uniqueFrameSets.add(cols[0].hashCode());
 				}
 				linenumber++;
 			}
+			csvw.flush();
+			csvw.close();
 			br.close();
 			fos.flush();
 			fos.close();
@@ -95,6 +113,18 @@ public class Analysis {
 		String line;
 		for (int frameNumber = 0; (line = br.readLine()) != null; frameNumber++) {
 			result.put(line.trim(), BitMask.longBitMask(frameNumber, numberOfFrames));
+		}
+		br.close();
+		return result;
+	}
+
+	private static String[] getHeader(String frameListPath) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(new File(frameListPath)));
+		int numberOfFrames = (int) FileUtils.countNumberOfLines(frameListPath);
+		String[] result = new String[numberOfFrames];
+		String line;
+		for (int frameNumber = numberOfFrames - 1; (line = br.readLine()) != null; frameNumber--) {
+			result[frameNumber] = line.trim();
 		}
 		br.close();
 		return result;
